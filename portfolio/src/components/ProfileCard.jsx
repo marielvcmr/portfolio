b9d3fbe213e50009d9cfcd0cf1b0b4fb2a922bfc
Marielv Chiquinquira Mavarez Rangel <marielvmavarez@gmail.com>
2026-08-07 23:38:56 -0400

    feat: profile card for homepage

---
+++ portfolio/src/components/ProfileCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ProfileCard.css';
+
+export default function ProfileCard() {
+  return (
+    <div className="profile-card-container">
+      <h2 className="profile-greeting">Hello, I'm Marielv !</h2>
+      <p className="profile-description">
+        Computer Engineerig Student on my 10th trimester. I have a strong background in programming, logic and data structures. My academic career has been focused on systems development, specifically in web environments. I’d like to keep learning about new and different technologies to create better solutions to problems through programming.
+      </p>
+    </div>
+  );
+}
--- portfolio/src/pages/HomePage.jsx
+++ portfolio/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-//import ProfileCard from './ProfileCard';
+import ProfileCard from '../components/ProfileCard';
 //import TechStack from './TechStack';
 //import Footer from './Footer';
 import './HomePage.css';
@@ -9,6 +9,10 @@ export default function HomePage({ onLogout }) {
   return (
     <div className="homepage-wrapper" id="home">
       <Navbar onLogout={onLogout} />
+
+      <main className="homepage-main-content">
+        <ProfileCard />
+      </main>
       
     </div>
   );
